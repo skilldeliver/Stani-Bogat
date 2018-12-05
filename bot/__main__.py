e10081f78be4a5a68404fdc0ee93ad3c714dd5d4
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-12-05 19:33:44 +0200

    haha

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,7 +4,7 @@ from bot.model import Bot
 def main():
     bot = Bot(prefix='*')
     bot.load_cogs()
-    bot.run('NTEzNDMzMjcxMDUzNDUxMjY1.Dt7E7w.C4maeBGQHEnQLHw-ZIjkF3LM3Rs')
+    bot.run('none.Dt7E7w.C4maeBGQHEnQLHw-ZIjkF3LM3Rs')
 
 
 if __name__ == '__main__':
