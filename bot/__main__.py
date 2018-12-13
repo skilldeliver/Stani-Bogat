7064f4681a84b11e3e898a39bb1239197ebaaddd
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-12-13 23:26:00 +0200

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,7 +5,7 @@ def main():
     bot = Bot(prefix=P,
               activity=f"{P}команди")
     bot.load_cogs()
-    bot.run('NDU0MjIzNjg1MDEyOTQ2OTQ0.DvK6qQ.pznDOEeJLk1RiMfcSpnGbazNnBY')
+    bot.run('opa')
 
 
 if __name__ == '__main__':
