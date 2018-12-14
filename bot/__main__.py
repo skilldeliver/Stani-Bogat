a69ef90f673ae6dd20242ab7ca05efd9c6f890ab
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-12-14 17:15:20 +0200

    back the pipfiles

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,7 +5,7 @@ def main():
     bot = Bot(prefix=P,
               activity=f"{P}команди")
     bot.load_cogs()
-    bot.run('none')
+    bot.run('NDU0MjIzNjg1MDEyOTQ2OTQ0.DvVZhA.v6eV1L2rXMlw9e_x7G2TqHPgNQ0')
 
 if __name__ == '__main__':
     main()
