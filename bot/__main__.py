798e68207f74ee0de3d0b97f5dfe4982a58cea55
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-12-14 17:33:41 +0200

    env variable

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,11 +1,13 @@
+import os
+
 from bot.core.constants import PREFIX as P
 from bot.core.model import Bot
 
 def main():
     bot = Bot(prefix=P,
               activity=f"{P}команди")
-    bot.load_cogs()
-    bot.run('NDU0MjIzNjg1MDEyOTQ2OTQ0.DvVZhA.v6eV1L2rXMlw9e_x7G2TqHPgNQ0')
+    bot.load_cogs(os.getenv('TOKEN'))
+    bot.run()
 
 if __name__ == '__main__':
     main()
